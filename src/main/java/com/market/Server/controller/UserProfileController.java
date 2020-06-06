532b2a28744c4c0efab90fc211507681c288ef41
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-06-07 02:52:47 +0900

    swagger library 추가 및 userProfile Controller에 연동

--- src/main/java/com/market/Server/controller/UserProfileController.java
+++ src/main/java/com/market/Server/controller/UserProfileController.java
@@ -2,10 +2,14 @@ package com.market.Server.controller;
 
 import com.market.Server.mapper.UserProfileMapper;
 import com.market.Server.repository.UserProfile;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. user"})
 @RestController
 public class UserProfileController {
 
@@ -21,13 +25,18 @@ public class UserProfileController {
         return mapper.getUserProfile(id);
     }
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping("/user/all")
     public List<UserProfile> getUserProfileList() {
         return mapper.getUserProfileList();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PutMapping("/user/{id}")
-    public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+    public void putUserProfile(@ApiParam(value = "회원아이디", required = true) @PathVariable("id") String id,
+                               @ApiParam(value = "회원이름", required = true) @RequestParam("name") String name,
+                               @ApiParam(value = "회원폰번호", required = true) @RequestParam("phone") String phone,
+                               @ApiParam(value = "회원주소", required = true) @RequestParam("address") String address) {
         mapper.insertUserProfile(id,name,phone,address);
     }
 
