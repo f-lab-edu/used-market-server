a7b335e6b019283941954fb92a3b6224b7ab73a5
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-06-15 23:52:42 +0900

    #6 코드 주석 추가
    @Service, @Component 차이및 스프링 동작 방식.

--- src/main/java/com/market/Server/service/ResponseService.java
+++ src/main/java/com/market/Server/service/ResponseService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 // 해당 Class가 서비스(서버에서 클라이언트에게 응답할때 response 패킷을 정의) 레이어 클래스라는 것을 spring framework에 정의한다.
+// spring은 데이터 엑세스, 프리젠테이션, 서비스, 비즈니스 등과 같은 고유 계층 클래스 경로를 스캐닝합니다. 
+// 그중 Component는 모든 Spring 관리 구성 요소로 분류합니다. Service는 서비스 계층에서 클래스에 주석을 답니다.
 // response를 class로 분리한 이유는 해당 클래스를 사용하는 곳에서 ExceptionHandler의 형태를 직접 정의한다.
 @Service
 public class ResponseService {
