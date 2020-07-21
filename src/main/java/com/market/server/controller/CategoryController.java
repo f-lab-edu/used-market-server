cb7173f5d03be70d17c8aa20e2da6af2a94c6dbb
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-07-21 14:28:20 +0900

    중고물품 카테고리 수정 및 삭제 개발

--- src/main/java/com/market/server/controller/CategoryController.java
+++ src/main/java/com/market/server/controller/CategoryController.java
@@ -2,9 +2,10 @@ package com.market.server.controller;
 
 import com.market.server.aop.LoginCheck;
 import com.market.server.dto.CategoryDTO;
-import com.market.server.dto.ProductDTO;
 import com.market.server.service.Impl.CategoryServiceImpl;
 import io.swagger.annotations.Api;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,37 @@ public class CategoryController {
     @LoginCheck(type = LoginCheck.UserType.ADMIN)
     public void registerCategory(String accountId, @RequestBody CategoryDTO categoryDTO) {
         categoryService.register(accountId, categoryDTO);
+    }
+
+    /**
+     * 중고물품 카테고리 수정 메서드.
+     */
+    @PatchMapping("{categoryId}/update")
+    @LoginCheck(type = LoginCheck.UserType.ADMIN)
+    public void updateCategories(String accountId,
+                               @PathVariable(name = "categoryId") int categoryId,
+                               @RequestBody CategoryRequest categoryRequest) {
+        CategoryDTO categoryDTO = new CategoryDTO(categoryId,categoryRequest.getName());
+        categoryService.update(categoryDTO);
+    }
+
+    /**
+     * 본인 중고물품 카테고리 삭제 메서드.
+     */
+    @DeleteMapping("{categoryId}/delete")
+    @LoginCheck(type = LoginCheck.UserType.ADMIN)
+    public void updateCategories(String accountId,
+                               @PathVariable(name = "categoryId") int categoryId) {
+        categoryService.delete(categoryId);
+    }
+
+    // -------------- request 객체 --------------
+
+    @Setter
+    @Getter
+    private static class CategoryRequest {
+        private int id;
+        private String name;
     }
 
 }
--- src/main/java/com/market/server/dto/CategoryDTO.java
+++ src/main/java/com/market/server/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.market.server.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class CategoryDTO {
     private int id;
     private String name;
