ed652f7d2c886b0047ecfc842ea48e2a30169bc2
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-09-12 17:07:06 +0900

    MainApplication Name update

--- src/test/java/com/market/server/UsedMarketServerApplicationTests.java
+++ src/test/java/com/market/server/UsedMarketServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-class ServerApplicationTests {
+class UsedMarketServerApplicationTests {
 
 	@Test
 	void contextLoads() {
