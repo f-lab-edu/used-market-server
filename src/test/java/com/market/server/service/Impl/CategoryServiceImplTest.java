73d86e62d175d632bf4d4809bdb86b087cf15b71
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-09-22 02:50:51 +0900

    #36 테스트 코드 작성 - CategoryServiceImplTest api 메서드 작성

---
+++ src/test/java/com/market/server/service/Impl/CategoryServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.market.server.service.Impl;
+
+import com.market.server.dao.ProductDao;
+import com.market.server.dto.CategoryDTO;
+import com.market.server.dto.ProductDTO;
+import com.market.server.mapper.CategoryMapper;
+import com.market.server.mapper.ProductMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(MockitoJUnitRunner.class)
+class CategoryServiceImplTest {
+
+    @InjectMocks
+    CategoryServiceImpl categoryService;
+
+    @Mock
+    CategoryMapper categoryMapper;
+
+    // 새로운 카테고리 객체를 생성하여 반환한다.
+    public CategoryDTO generateCategory() {
+        MockitoAnnotations.initMocks(this); // mock all the field having @Mock annotation
+        CategoryDTO categoryDTO = CategoryDTO.builder()
+                .id(1)
+                .name("testName")
+                .sortStatus(CategoryDTO.SortStatus.NEWEST)
+                .searchCount(1000)
+                .pagingStartOffset(0)
+                .build();
+        return categoryDTO;
+    }
+
+    @Test
+    void register() {
+        CategoryDTO categoryDTO = generateCategory();
+        given(categoryMapper.register(categoryDTO)).willReturn(categoryDTO.getId());
+        categoryMapper.register(categoryDTO);
+    }
+
+    @Test
+    void update() {
+        CategoryDTO categoryDTO = generateCategory();
+        categoryDTO.setName("testName2");
+        categoryMapper.updateCategory(categoryDTO);
+        assertTrue(categoryDTO.getName().equals("testName2"));
+    }
+
+    @Test
+    void delete() {
+        CategoryDTO categoryDTO = generateCategory();
+        categoryDTO.setId(1);
+        categoryMapper.deleteCategory(1);
+    }
+}
